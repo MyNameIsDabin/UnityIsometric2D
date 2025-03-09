12cde45fb97bcc82fbc1ecebed6e3de168ba5094
Dabin <dorothy9795@gmail.com>
2025-03-09 23:05:28 +0900

    isometric 개체들끼리의 충돌 체크

--- Assets/Isometric2D/IsometricObject.cs
+++ Assets/Isometric2D/IsometricObject.cs
@@ -1,45 +1,114 @@
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
-[ExecuteAlways]
-public class IsometricBody : MonoBehaviour
+namespace Isometric2D
 {
-    [SerializeField] private Vector2 extends;
-    [SerializeField] private float virtual3dHeight;
-    
-    private void OnDrawGizmos()
+    [ExecuteAlways]
+    public class IsometricObject : MonoBehaviour
     {
-        DrawIsometricBody();
-    }
+        [SerializeField] private Vector2 extends;
+        [SerializeField] private float height;
+        
+        private Color _gizmosColor = Color.white;
 
-    private void DrawIsometricBody()
-    {
-        var isometricWorld = IsometricWorld.Instance;
+        // Top, Right, Bottom, Left
+        private Vector3[] _floorCorners = new Vector3[4];
 
-        if (isometricWorld == null)
-            return;
-        
-        var previousColor = Gizmos.color;
-        
-        Gizmos.color = isometricWorld.TileColor;
-        
-        var floorCorners = isometricWorld.GetIsometricCorners(transform.position, extends);
-        
-        if (virtual3dHeight > 0)
+        public float Height => height;
+        public Vector3 FloorTopCorner => _floorCorners[0];
+        public Vector3 FloorRightCorner => _floorCorners[1];
+        public Vector3 FloorBottomCorner => _floorCorners[2];
+        public Vector3 FloorLeftCorner => _floorCorners[3];
+
+        public Vector2[] Corners { get; private set; } = new Vector2[6];
+
+        private void OnEnable()
         {
-            var topCorners = isometricWorld.GetIsometricCorners(transform.position + Vector3.up * virtual3dHeight, extends);
-            isometricWorld.DrawIsometricTile(floorCorners, Color.grey, Color.white, Color.white, Color.grey);
-            isometricWorld.DrawIsometricTile(topCorners);
+            IsometricWorld.Instance.AddIsometricObject(this);
+        }
+
+        private void OnValidate()
+        {
+            IsometricWorld.Instance.AddIsometricObject(this);
+        }
+
+        private void OnDisable()
+        {
+            IsometricWorld.Instance.RemoveIsometricObject(this);
+        }
+
+        private void OnDrawGizmos()
+        {
+            DrawIsometricBody();
+        }
+
+        private void Update()
+        {
+            var isometricWorld = IsometricWorld.Instance;
+
+            if (isometricWorld == null)
+                return;
             
-            Gizmos.DrawLine(floorCorners[1], topCorners[1]);
-            Gizmos.DrawLine(floorCorners[2], topCorners[2]);
-            Gizmos.DrawLine(floorCorners[3], topCorners[3]);
+            _floorCorners = isometricWorld.GetIsometricCorners(transform.position, extends);
+
+            var virtualHeight = Vector3.up * height;
+            
+            Corners[0] = _floorCorners[0] + virtualHeight; // Top
+            Corners[1] = _floorCorners[1] + virtualHeight; // Right Top
+            Corners[2] = _floorCorners[1]; // Right Bottom
+            Corners[3] = _floorCorners[2]; // Bottom
+            Corners[4] = _floorCorners[3]; // Left Bottom
+            Corners[5] = _floorCorners[3] + virtualHeight; // Left Top
+            
+            var contacted = false;
+            
+            foreach (var isometricObject in isometricWorld.IsometricObjects)
+            {
+                if (isometricObject == this)
+                    continue;
+                
+                if (!isometricObject.gameObject.activeSelf)
+                    continue;
+                
+                if (isometricObject.IsOverlap(this))
+                {
+                    _gizmosColor = Color.green;
+                    contacted = true;
+                    break;
+                }
+            }
+
+            if (!contacted)
+                _gizmosColor = isometricWorld.TileColor;
         }
-        else
+
+        private void DrawIsometricBody()
         {
-            isometricWorld.DrawIsometricTile(floorCorners);
+            var isometricWorld = IsometricWorld.Instance;
+
+            if (isometricWorld == null || _floorCorners == null)
+                return;
+
+            var previousColor = Gizmos.color;
+
+            Gizmos.color = _gizmosColor;
+
+            if (height > 0)
+            {
+                var topCorners = isometricWorld.GetIsometricCorners(transform.position + Vector3.up * height, extends);
+                isometricWorld.DrawIsometricTile(_floorCorners, Color.grey, _gizmosColor, _gizmosColor, Color.grey);
+                isometricWorld.DrawIsometricTile(topCorners);
+
+                Gizmos.DrawLine(_floorCorners[1], topCorners[1]);
+                Gizmos.DrawLine(_floorCorners[2], topCorners[2]);
+                Gizmos.DrawLine(_floorCorners[3], topCorners[3]);
+            }
+            else
+            {
+                isometricWorld.DrawIsometricTile(_floorCorners);
+            }
+
+            Gizmos.color = previousColor;
         }
-        
-        Gizmos.color = previousColor;
     }
 }
--- Assets/Isometric2D/IsometricWorld.cs
+++ Assets/Isometric2D/IsometricWorld.cs
@@ -1,136 +1,154 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
-public class IsometricWorld : MonoBehaviour
+namespace Isometric2D
 {
-    [Header("Isometric Settings")]
-    [SerializeField] private float tileWidth = 1f;
-    [SerializeField] private float tileHeight = 0.5f;
-
-    [Header("Debug")]
-    [SerializeField] private Color tileColor = Color.white;
-    
-    private Vector3[] _cachedIsometricIdentityCorners;
-    private Vector3 _cachedDirectionToRightTop;
-    private Vector3 _cachedDirectionToLeftTop;
-    private Vector3 _cachedDirectionToLeftBottom;
-    private Vector3 _cachedDirectionToRightBottom;
-    
-    public Color TileColor => tileColor;
-
-    private static IsometricWorld _instance;
-    
-    public static IsometricWorld Instance
+    [ExecuteAlways]
+    public class IsometricWorld : MonoBehaviour
     {
-        get
+        [Header("Isometric Settings")] [SerializeField]
+        private float tileWidth = 1f;
+
+        [SerializeField] private float tileHeight = 0.5f;
+
+        [Header("Debug")] [SerializeField] private Color tileColor = Color.white;
+
+        private Vector3[] _cachedIsometricIdentityCorners;
+        private Vector3 _cachedDirectionToRightTop;
+        private Vector3 _cachedDirectionToLeftTop;
+        private Vector3 _cachedDirectionToLeftBottom;
+        private Vector3 _cachedDirectionToRightBottom;
+
+        public Color TileColor => tileColor;
+
+        private List<IsometricObject> _isometricObjects = new();
+        public List<IsometricObject> IsometricObjects => _isometricObjects;
+        
+        private static IsometricWorld _instance;
+
+        public static IsometricWorld Instance
         {
-            if (_instance != null) 
-                return _instance;
-            
-            _instance = FindObjectOfType<IsometricWorld>();
-            
-            if (_instance != null) 
-                return _instance;
-            
-            var inst = new GameObject("Isometric World").AddComponent<IsometricWorld>();
-
-            Debug.Log("Isometric World Created.", inst);
-            return inst;
-        }
-    }
-    
-    private void Awake()
-    {
-        _instance = this;
-    }
-    
-    public Vector3[] IsometricIdentityCorners
-    {
-        get
-        {
-            if (_cachedIsometricIdentityCorners == default 
-                || _cachedIsometricIdentityCorners.Length == 0)
+            get
             {
-                _cachedIsometricIdentityCorners = new[]
-                {
-                    new Vector3(0, tileHeight / 2f, 0), // Top
-                    new Vector3(tileWidth / 2f, 0, 0), // Right
-                    new Vector3(0, -tileHeight / 2f, 0), // Bottom
-                    new Vector3(-tileWidth / 2f, 0, 0) // Left
-                };
+                if (_instance != null)
+                    return _instance;
+
+                _instance = FindObjectOfType<IsometricWorld>();
+
+                if (_instance != null)
+                    return _instance;
+
+                var inst = new GameObject("Isometric World").AddComponent<IsometricWorld>();
+
+                Debug.Log("Isometric World Created.", inst);
+                return inst;
             }
-            
-            return _cachedIsometricIdentityCorners;
-        }   
-    }
-
-    public Vector3 DirectionToRightTop
-    {
-        get
-        {
-            if (_cachedDirectionToRightTop == default)
-                _cachedDirectionToRightTop = (IsometricIdentityCorners[0] - IsometricIdentityCorners[3]).normalized;
-            return _cachedDirectionToRightTop;
-        }
-    }
-
-    public Vector3 DirectionToLeftTop
-    {
-        get
-        {
-            if (_cachedDirectionToLeftTop == default)
-                _cachedDirectionToLeftTop = (IsometricIdentityCorners[0] - IsometricIdentityCorners[1]).normalized;
-            return _cachedDirectionToLeftTop;
-        }
-    }
-
-    public Vector3 DirectionToLeftBottom
-    {
-        get
-        {
-            if (_cachedDirectionToLeftBottom == default)
-                _cachedDirectionToLeftBottom = (IsometricIdentityCorners[2] - IsometricIdentityCorners[1]).normalized;
-            return _cachedDirectionToLeftBottom;
-        }
-    }
-
-    public Vector3 DirectionToRightBottom
-    {
-        get
-        {
-            if (_cachedDirectionToRightBottom == default)
-                _cachedDirectionToRightBottom = (IsometricIdentityCorners[1] - IsometricIdentityCorners[0]).normalized;
-            return _cachedDirectionToRightBottom;
-        }
-    }
-    
-    public Vector3[] GetIsometricCorners(Vector3 worldPosition, Vector2 extends)
-    {
-        var extendToRightTop = (IsometricIdentityCorners[0] - IsometricIdentityCorners[3]) * (extends[0] - 1.0f);
-        var extendToLeftTop = (IsometricIdentityCorners[0] - IsometricIdentityCorners[1]) * (extends[1] - 1.0f);
-        var identity = IsometricIdentityCorners;
-        
-        return new[]
-        {
-            identity[0] + worldPosition + extendToRightTop + extendToLeftTop, // Top
-            identity[1] + worldPosition + extendToRightTop, // Right
-            identity[2] + worldPosition, // Bottom
-            identity[3] + worldPosition + extendToLeftTop // Left
-        };
-    }
-    
-    public void DrawIsometricTile(Vector3[] vertices, params Color[] colors)
-    {
-        var previousColor = Gizmos.color;
-        
-        for (var i = 0; i < 4; i++)
-        {
-            Gizmos.color = colors.Length > i ? colors[i] : previousColor;
-            var start = vertices[i];
-            var end = vertices[(i + 1) % 4];
-            Gizmos.DrawLine(start, end);
         }
 
-        Gizmos.color = previousColor;
+        private void Awake()
+        {
+            _instance = this;
+        }
+
+        public void AddIsometricObject(IsometricObject isometricObject)
+        {
+            if (!_isometricObjects.Contains(isometricObject))
+                _isometricObjects.Add(isometricObject);
+        }
+
+        public void RemoveIsometricObject(IsometricObject isometricObject)
+        {
+            _isometricObjects.Remove(isometricObject);
+        }
+
+        public Vector3[] IsometricIdentityCorners
+        {
+            get
+            {
+                if (_cachedIsometricIdentityCorners == default
+                    || _cachedIsometricIdentityCorners.Length == 0)
+                {
+                    _cachedIsometricIdentityCorners = new[]
+                    {
+                        new Vector3(0, tileHeight / 2f, 0), // Top
+                        new Vector3(tileWidth / 2f, 0, 0), // Right
+                        new Vector3(0, -tileHeight / 2f, 0), // Bottom
+                        new Vector3(-tileWidth / 2f, 0, 0) // Left
+                    };
+                }
+
+                return _cachedIsometricIdentityCorners;
+            }
+        }
+
+        public Vector3 IsoIdentityDirectionToRightTop
+        {
+            get
+            {
+                if (_cachedDirectionToRightTop == default)
+                    _cachedDirectionToRightTop = (IsometricIdentityCorners[0] - IsometricIdentityCorners[3]);
+                return _cachedDirectionToRightTop;
+            }
+        }
+
+        public Vector3 IsoIdentityDirectionToLeftTop
+        {
+            get
+            {
+                if (_cachedDirectionToLeftTop == default)
+                    _cachedDirectionToLeftTop = (IsometricIdentityCorners[0] - IsometricIdentityCorners[1]);
+                return _cachedDirectionToLeftTop;
+            }
+        }
+
+        public Vector3 IsoIdentityDirectionToLeftBottom
+        {
+            get
+            {
+                if (_cachedDirectionToLeftBottom == default)
+                    _cachedDirectionToLeftBottom = (IsometricIdentityCorners[2] - IsometricIdentityCorners[1]);
+                return _cachedDirectionToLeftBottom;
+            }
+        }
+
+        public Vector3 IsoIdentityDirectionToRightBottom
+        {
+            get
+            {
+                if (_cachedDirectionToRightBottom == default)
+                    _cachedDirectionToRightBottom = (IsometricIdentityCorners[1] - IsometricIdentityCorners[0]);
+                return _cachedDirectionToRightBottom;
+            }
+        }
+
+        public Vector3[] GetIsometricCorners(Vector3 worldPosition, Vector2 extends)
+        {
+            var extendToRightTop = IsoIdentityDirectionToRightTop * (extends[0] - 1.0f);
+            var extendToLeftTop = IsoIdentityDirectionToLeftTop * (extends[1] - 1.0f);
+            var identity = IsometricIdentityCorners;
+
+            return new[]
+            {
+                identity[0] + worldPosition + extendToRightTop + extendToLeftTop, // Top
+                identity[1] + worldPosition + extendToRightTop, // Right
+                identity[2] + worldPosition, // Bottom
+                identity[3] + worldPosition + extendToLeftTop // Left
+            };
+        }
+
+        public void DrawIsometricTile(Vector3[] vertices, params Color[] colors)
+        {
+            var previousColor = Gizmos.color;
+
+            for (var i = 0; i < 4; i++)
+            {
+                Gizmos.color = colors.Length > i ? colors[i] : previousColor;
+                var start = vertices[i];
+                var end = vertices[(i + 1) % 4];
+                Gizmos.DrawLine(start, end);
+            }
+
+            Gizmos.color = previousColor;
+        }
     }
 }
